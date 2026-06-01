323176d6e7fcfe2a0522cdca946186172843e9bb
theketan26 <ketangsolanki129@gmail.com>
2026-06-02 01:22:19 +0530

    feat: umami added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Exo_2, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/components/common/ReduxProvider";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const geistSans = Exo_2({
   variable: "--font-geist-sans",
@@ -33,6 +34,11 @@ export default function RootLayout({
         <Analytics />
         <ReduxProvider>{children}</ReduxProvider>
       </body>
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="16f69bab-1528-4626-924c-1a8e58ef5d14"
+      />
     </html>
   );
 }
